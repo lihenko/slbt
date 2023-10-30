9b8be823c786a62897233142aa7cbcd6b7fcab86
Vladyslav Lykhenko <obbdpu@gmail.com>
2023-10-30 12:42:00 +0200

    SLBT

--- src/js/main.js
+++ src/js/main.js
@@ -29,7 +29,7 @@ function backToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
-$('.inshop-services-slider').slick({
+jQuery('.inshop-services-slider').slick({
   dots: false,
   arrows: true,
   infinite: true,
@@ -63,6 +63,47 @@ $('.inshop-services-slider').slick({
         arrows:false,
         dots:true
       }
+    }
+  ]
+});
+
+jQuery('.seals-slider').slick({
+  dots: false,
+  arrows: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1250,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+       }
     }
   ]
 });
